b7fe5f6977cbb2172c1e8c1f4695220864b34912
Juriel Botoy <juriel.botoy182@gmail.com>
2022-12-19 15:12:43 +0800

    [GPT3-005] - Used argparse for parameterizing inputs

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,9 +1,23 @@
 import openai
 import os
+import argparse
 
-OPENAI_API_KEY = 'OPENAI_API_KEY'
+OPENAI_API_KEY = "OPENAI_API_KEY"
+
 
 def main():
+    parser = argparse.ArgumentParser(description="PyChat v0.1")
+    parser.add_argument("--max-tokens", help="Maximum size of tokens to be used.", type=int, default=4000)
+    parser.add_argument("--engine", help="The OpenAI engine to use", type=str, default='text-davinci-003')
+    args = parser.parse_args()
+
+    max_tokens = args.max_tokens
+    engine = args.engine
+
+    print("Options: ")
+    print("max_tokens: {}".format(max_tokens))
+    print("engine: {}".fomat(engine))
+    
     openpi_api_key = os.getenv(OPENAI_API_KEY)
     if openpi_api_key == None:
         print("OPENAI_API_KEY required")
