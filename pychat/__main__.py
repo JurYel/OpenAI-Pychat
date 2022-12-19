5d311fb6ef09fac04d342f90e6403ebe3aa3fcf5
Juriel Botoy <juriel.botoy182@gmail.com>
2022-12-19 11:26:54 +0800

    [GPT3-003] - Removed commented code

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -8,12 +8,10 @@ def main():
     if openpi_api_key == None:
         print("OPENAI_API_KEY required")
         exit(-1)
-        
+
     print("PyChat v0.1")
     # openai.api_key = "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"
     query = input("Input: ")
-
-    # print("Query: {}".format(query))
 
     completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens=4000)
     output = completion.choices[0].text
