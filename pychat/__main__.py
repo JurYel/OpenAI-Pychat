441139cfe362ba51beb6fb0e69522984ef9cbaaf
Juriel Botoy <juriel.botoy182@gmail.com>
2022-12-19 10:59:54 +0800

    [GPT3-002] - Added readme file

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,13 +1,3 @@
-# TODO:
-# 1. Handle detection of OPENAI_API_KEY availability
-# 2. max_tokens can be set as a parameter outside
-# 3. empty input from user. Suggested to use try-except
-# 4. engine be set as a parameter with a default of 'text-davinci-03'
-# 5. choices should be checked if we actually have values
-
-# Additional Features:
-# 1. Put code in while loop so you won't have to rerun program
-
 import openai
 
 def main():
